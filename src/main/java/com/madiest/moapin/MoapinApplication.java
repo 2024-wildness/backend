1f284ab34588fe333a7aa1071a0ba7ff44708662
김윤기 <kalsagansteve@gmail.com>
2025-07-31 14:12:16 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/madiest/moapin/MoapinApplication.java
@@ -0,0 +1,13 @@
+package com.madiest.moapin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoapinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoapinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madiest/moapin/MoapinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madiest.moapin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoapinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
